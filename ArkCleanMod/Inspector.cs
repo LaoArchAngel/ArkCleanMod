18dfeb22fc934c8ceb0370dfbdb1ccfefa37b73c
Lao <lao@nexverse.net>
2017-05-06 15:29:31 -0600

    Started work on the inspector

--- ArkCleanMod/Inspector.cs
+++ ArkCleanMod/Inspector.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ArkCleanMod
 {
@@ -20,11 +22,45 @@ namespace ArkCleanMod
             if(!BaseDir.Exists)
                 throw new ArgumentException("The base directory does for steam mods does not exist.  Please supply a valid path.", basePath);
 
-            if (!BaseDir.GetDirectories("workshop").Any() || !BaseDir.GetDirectories("common").Any())
+            bool foundWorkshop = BaseDir.GetDirectories("workshop").Any();
+            bool foundCommon = BaseDir.GetDirectories("common").Any();
+
+            if (!(foundWorkshop && foundCommon))
             {
                 throw new ArgumentException(
                     "The base directory does not appear to be the correct path.  Please find your steamapps folder and try again.  The steamapps folder is generally located directly inside the steam install directory (eg. C:\\Program Files (x86)\\Steam\\steamapps)");
             }
+        }
+
+        public void Start()
+        {
+            while (!stopCalled)
+            {
+                
+            }
+        }
+
+        Task Scan(CancellationToken cancel)
+        {
+            DirectoryInfo downloads = new DirectoryInfo(Path.Combine(BaseDir.FullName, @"workshop\content\346110"));
+
+            if (!downloads.Exists)
+                return null;
+
+            foreach (DirectoryInfo modDownload in downloads.EnumerateDirectories())
+            {
+                DirectoryInfo modInstall = FindInstallFolder(modDownload.Name);
+
+                if (modInstall != null)
+                {
+                    
+                }
+            }
+        }
+
+        private DirectoryInfo FindInstallFolder(string modId)
+        {
+            DirectoryInfo installFolder = new DirectoryInfo(Path.Combine(BaseDir.FullName, @""));
         }
     }
 }
