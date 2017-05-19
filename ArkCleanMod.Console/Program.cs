21294230c1ffb0bfd4b6abdda092b841d128bd0e
Lao <lao@nexverse.net>
2017-05-19 03:42:30 -0600

    Added console runner

---
+++ ArkCleanMod.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ArkCleanMod.Console
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var installed = new DirectoryInfo(@"C:\Program Files (x86)\Steam\steamapps\common\ARK\ShooterGame\Content\Mods");
+            var downloaded = new DirectoryInfo(@"C:\Program Files (x86)\Steam\steamapps\workshop\content\346110");
+            long modId = 771279262;
+            var mod = new ModInfo(installed, downloaded, modId);
+            var mods = new Dictionary<long, ModInfo>
+            {
+                {modId, mod}
+            };
+            var cleaner = new Processors.InstalledCleaner();
+            Task.Run(() => cleaner.ProcessModsAsync(mods)).GetAwaiter().GetResult();
+        }
+    }
+}
