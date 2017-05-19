68d64e4690e82487e75a452d4b5ad1a78d503522
Lao <lao@nexverse.net>
2017-05-19 03:18:03 -0600

    Initial writing of InstalledCleaner processor.

---
+++ ArkCleanMod/Processors/InstalledCleaner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ArkCleanMod.Specifications.ModUpdateTimes;
+
+namespace ArkCleanMod.Processors
+{
+    public class InstalledCleaner : IModProcessor
+    {
+        /// <summary>
+        ///   Deletes the files from mod folders that were already installed.
+        /// </summary>
+        /// <param name="mods">List of all mods found</param>
+        /// <returns>A task cleaning all mod folders that have been installed.</returns>
+        public Task ProcessModsAsync(IDictionary<long, ModInfo> mods)
+        {
+            var spec = new DownloadedNeedsCleanup();
+            IEnumerable<ModInfo> toClean = mods.Values.Where(spec.IsSatisfiedBy);
+            var tasks = new List<Task>();
+
+            foreach (ModInfo mod in toClean)
+            {
+                tasks.Add(Task.Run(() => CleanMod(mod)));
+            }
+
+            return Task.WhenAll(tasks);
+        }
+
+        private void CleanMod(ModInfo toClean)
+        {
+            DirectoryInfo downloadDir = toClean.DownloadDir;
+
+            foreach (FileSystemInfo fileSystemInfo in downloadDir.EnumerateFileSystemInfos())
+            {
+                if (!IsKeyFile(fileSystemInfo))
+                {
+                    fileSystemInfo.Delete();
+                }
+            }
+        }
+
+        private bool IsKeyFile(FileSystemInfo file)
+        {
+            if (!(file is FileInfo))
+                return false;
+
+            if (file.Extension != "info")
+                return false;
+
+            return file.Name == "mod" || file.Name == "modmeta";
+        }
+    }
+}
