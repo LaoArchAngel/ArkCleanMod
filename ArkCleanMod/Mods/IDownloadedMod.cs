6e3e8b30f5482522ad6e9266703eae1eff4e622f
Lao <lao@nexverse.net>
2017-05-22 02:28:50 -0600

    Introduced new interfaces for downloaded and installed mods.
    Adding testability.

---
+++ ArkCleanMod/Mods/IDownloadedMod.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ArkCleanMod.Mods
+{
+    public interface IDownloadedMod
+    {
+        DateTime DownloadUpdate { get; }
+        bool DownloadHasFiles { get; }
+    }
+}
---
+++ ArkCleanMod/Mods/IInstalledMod.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace ArkCleanMod.Mods {
+    public interface IInstalledMod {
+        DateTime? InstallUpdate { get; }
+    }
+}
