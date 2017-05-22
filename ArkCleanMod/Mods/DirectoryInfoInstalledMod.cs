08a149f124c9984730690e32c5b22249593e72ae
Lao <lao@nexverse.net>
2017-05-22 02:40:14 -0600

    Added DirectoryInfoInstalledMod
    Implementation of IInstalledMod based on DirectoryInfo
    Added IsInstalled property to IInstalledMod and DirectoryInfoInstalledMod

---
+++ ArkCleanMod/Mods/DirectoryInfoInstalledMod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace ArkCleanMod.Mods
+{
+    /// <inheritdoc />
+    public class DirectoryInfoInstalledMod : IInstalledMod
+    {
+        private readonly FileInfo _dotModFile;
+        private readonly DirectoryInfo _installDir;
+
+        internal DirectoryInfoInstalledMod(string installPath, long modId)
+        {
+            _installDir = new DirectoryInfo(Path.Combine(installPath, modId.ToString()));
+            _dotModFile = new FileInfo(Path.Combine(installPath, $"{modId}.info"));
+        }
+
+        /// <inheritdoc />
+        public bool IsInstalled
+        {
+            get { return _installDir.Exists }
+        }
+
+        /// <inheritdoc />
+        public DateTime? InstallUpdate
+        {
+            get
+            {
+                if (!(_installDir.Exists && _dotModFile.Exists))
+                {
+                    return null;
+                }
+
+                return _installDir.LastWriteTimeUtc;
+            }
+        }
+    }
+}
--- ArkCleanMod/Mods/IInstalledMod.cs
+++ ArkCleanMod/Mods/IInstalledMod.cs
@@ -3,5 +3,10 @@
 namespace ArkCleanMod.Mods {
     public interface IInstalledMod {
         DateTime? InstallUpdate { get; }
+
+        /// <summary>
+        ///     Gets whether or not the mod is installed.
+        /// </summary>
+        bool IsInstalled { get; }
     }
 }
