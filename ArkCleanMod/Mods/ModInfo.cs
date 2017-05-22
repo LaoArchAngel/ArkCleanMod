881627888f953b246362e5d2689f328f287df0a4
Lao <lao@nexverse.net>
2017-05-22 15:52:58 -0600

    Refactored ModInfo
    Now uses IInstalledMod and IDownloadMod instead of having all logic.
    Composition over Inheritance
    Interface Segregation Principle

---
+++ ArkCleanMod/Mods/ModInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArkCleanMod.Mods
+{
+    public class ModInfo
+    {
+        private readonly IInstalledMod _installedMod;
+
+        private readonly IDownloadedMod _downloadedMod;
+
+        public ModInfo(long modId, IDownloadedMod downloaded, IInstalledMod installed)
+        {
+            _downloadedMod = downloaded;
+            _installedMod = installed;
+            ModId = modId;
+        }
+
+        public long ModId { get; }
+
+        public DateTime DownloadUpdate => _downloadedMod.DownloadUpdate;
+
+        public DateTime? InstallUpdate => _installedMod?.InstallUpdate;
+
+        public bool DownloadHasFiles => _downloadedMod.DownloadHasFiles;
+    }
+}
