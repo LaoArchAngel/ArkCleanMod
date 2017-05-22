2e1867e7a6f816b91ce21d5645d4403cca83189b
Lao <lao@nexverse.net>
2017-05-22 02:30:29 -0600

    Added DirectoryInfoDownloadedModFactory
    Implementation of IDownloadedModFactory that instantiates DirectoryInfoDownloadedMod.

---
+++ ArkCleanMod/Mods/Factories/DirectoryInfoDownloadedModFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace ArkCleanMod.Mods.Factories
+{
+    public class DirectoryInfoDownloadedModFactory : IDownloadedModFactory
+    {
+        private readonly string _downloadPath;
+
+        public DirectoryInfoDownloadedModFactory(string downloadPath)
+        {
+            _downloadPath = downloadPath;
+
+            var downloadsFolder = new DirectoryInfo(_downloadPath);
+
+            if (!downloadsFolder.Exists)
+            {
+                throw new ArgumentException("Download path does not exist.");
+            }
+        }
+
+        public IDownloadedMod GetDownloadedMod(long modId)
+        {
+            return new DirectoryInfoDownloadedMod(_downloadPath, modId);
+        }
+    }
+}
